ced5c6b748992a6e9faba363f842325db685d928
KOSAKA Gemma <gemmaro.dev@gmail.com>
2023-01-03 23:03:29 +0900

    [ fix ] Lua script loading

--- lib/djot.rb
+++ lib/djot.rb
@@ -2,46 +2,49 @@
 
 require_relative "djot/version"
 require "language/lua"
+require "pathname"
 
 module Djot
   class Error < StandardError; end
 
-  LUA = Language::Lua.new.tap do |lua|
-    Dir.chdir(__dir__) do
-      lua.eval(<<~END_LUA)
-        function djot_parser(input)
-          local parser = require("djot").Parser:new(input)
-          parser:parse()
-          return parser
-        end
+  LUA = Language::Lua.new
+  LUA.eval(<<~END_LUA)
+    function djot_parser(input)
+      local parser = require("djot").Parser:new(input)
+      parser:parse()
+      return parser
+    end
 
-        function djot_render_html(input)
-          return djot_parser(input):render_html()
-        end
+    function djot_render_html(input)
+      return djot_parser(input):render_html()
+    end
 
-        function djot_render_matches(input)
-          return djot_parser(input):render_matches()
-        end
+    function djot_render_matches(input)
+      return djot_parser(input):render_matches()
+    end
 
-        function djot_render_ast(input)
-          return djot_parser(input):render_ast()
-        end
-      END_LUA
-    rescue => e
-      p Dir.pwd
-      raise e
+    function djot_render_ast(input)
+      return djot_parser(input):render_ast()
+    end
+  END_LUA
+
+  ROOT = Pathname(__dir__) / "lua"
+
+  def self.render_html(input)
+    Dir.chdir(ROOT) do
+      LUA.djot_render_html(input)
     end
   end
 
-  def self.render_html(input)
-    LUA.djot_render_html(input)
-  end
-
   def self.render_matches(input)
-    LUA.djot_render_matches(input)
+    Dir.chdir(ROOT) do
+      LUA.djot_render_matches(input)
+    end
   end
 
   def self.render_ast(input)
-    LUA.djot_render_ast(input)
+    Dir.chdir(ROOT) do
+      LUA.djot_render_ast(input)
+    end
   end
 end
